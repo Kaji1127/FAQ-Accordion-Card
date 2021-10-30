3aeed0a9a55b4cfbd2e15eb5e72ffe7d6f3e1f29
K.Kishin <riesgraf.birsack.1226@gmail.com>
2021-10-31 00:15:40 +0900

    edit style

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-
+const items=document.querySelectorAll(".faq__item"),descs=document.querySelectorAll(".faq__description"),arrow=document.querySelectorAll(".arrow");items.forEach((e=>{e.addEventListener("click",(()=>{e.classList.toggle("active")}))}));
 //# sourceMappingURL=script.js.map
